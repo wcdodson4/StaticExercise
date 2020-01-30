a227a1caa1b226d78077bd663389be1175b2a611
William Dodson <wcdodson4@gmail.com>
2020-01-30 09:27:47 -0600

    exercise complete

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TempConverter.FahrenheitToCelsius(32));
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(100));
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double ftemp)
+        {
+            return (ftemp - 32) / 1.8;
+        }
+
+        public static double CelsiusToFahrenheit(double ctemp)
+        {
+            return (ctemp * 1.8) + 32;
+        }
+    }
+}
